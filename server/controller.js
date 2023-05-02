287a1299d6807d933408f96b7a4f7ec11453ad58
Hunter Harris <hunter.alexander124@gmail.com>
2023-05-02 16:14:58 -0600

    Finished lab

---
+++ server/controller.js
@@ -0,0 +1,46 @@
+const houses = require('./db.json')
+let globalId = 4
+
+
+
+module.exports = {
+    getHouses: (req, res) => {
+        res.status(200).send(houses)
+    },
+    deleteHouse: (req, res) => {
+        let {id} = req.params
+        let index = houses.findIndex(elem => elem.id === +id)
+        houses.splice(index, 1)
+        res.status(200).send(houses)
+    },
+    createHouse: (req, res) => {
+        let {address, price, imageURL} = req.body
+        let newHouse = {
+                address,
+                price,
+                imageURL,
+                id: globalId
+        }
+        houses.push(newHouse)
+        res.status(200).send(houses)
+        globalId++
+    },
+    updateHouse: (req, res) => {
+        const {id} = req.params
+        const {type} = req.body
+
+        let index = houses.findIndex(elem => +elem.id === +id)
+
+        if (houses[index].price <= 10000 && type === 'minus'){
+            res.status(400).send('Houses cannot be less than $10,000')
+        }else if (type === "plus"){
+            houses[index].price += 10000
+            res.status(200).send(houses)
+        } else if (type === "minus"){
+            houses[index].price -= 10000
+            res.status(200).send(houses)
+        } else {
+            res.status(400)
+        }
+    }
+}
